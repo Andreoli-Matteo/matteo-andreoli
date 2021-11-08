24ab336d48f366ffa44be1ab330f407a210234d9
Andreoli-Matteo <andreoli.matteo@iisgalvanimi.edu.it>
2021-11-08 12:56:04 +0000

    DOM

---
+++ script.js
@@ -0,0 +1,8 @@
+function button1Function() {
+    document.getElementById("mytext").innerHTML = "Hello World";
+    document.getElementById("mytext2").innerHTML = "sono il secondo testo ";
+  
+  }
+  
+  
+  
