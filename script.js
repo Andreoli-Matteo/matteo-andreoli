604904a13538e8e2ef7e638e10cfb35177e6449b
Andreoli-Matteo <andreoli.matteo@iisgalvanimi.edu.it>
2021-11-18 10:56:52 +0000

    DOM

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function button1Function() {
     document.getElementById("mytext").innerHTML = "Hello World";
     document.getElementById("mytext2").innerHTML = "sono il secondo testo ";
-  
+    
+  }
+ function button2Function(){
+   console.log("ciao");
+    document.getElementById("text3").innerHTML= "mi piace javascript";
+    document.getElementById("text4").innerHTML= "non mi piace javascript";
   }
   
   
