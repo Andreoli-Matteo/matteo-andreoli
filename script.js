63486aae65ff1bcbc9aba7c978144e448ce06160
Andreoli-Matteo <andreoli.matteo@iisgalvanimi.edu.it>
2021-11-18 13:57:10 +0000

    preparazione alla verifica

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 function button1Function() {
-    document.getElementById("mytext").innerHTML = "Hello World";
-    document.getElementById("mytext2").innerHTML = "sono il secondo testo ";
-    
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(0, 0, 150, 75);
+  ctx.fillStyle = "#00FF00";
+
+  ctx.fillRect(0, 0, 10, 10);
+
+  ctx.fillRect(20, 20, 10, 10);
+}
+function button2Function(){
+  var canvas= document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.fillstyle="#00FF00";
+  for (let index=0;index=140;index=+20){
+  ctxfillRect(index,index,10,10)
   }
- function button2Function(){
-   console.log("ciao");
-    document.getElementById("text3").innerHTML= "mi piace javascript";
-    document.getElementById("text4").innerHTML= "non mi piace javascript";
-  }
+  for (let index=140;index=0;index=-20){
+   let z=140;
+   ctxfillRect(index,z-index,10,10)
+}
+
+
   
   
-  
+}
